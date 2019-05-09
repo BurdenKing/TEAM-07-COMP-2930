b4069d5901b813f81b0dfae51aeb71ccee3891ec
weihan123git <chouweihan@gmail.com>
2019-05-09 09:54:48 -0700

    added some more functionality to js

---
+++ Application/Javascript/createAccount.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    //check if user is logged, create function for this
+    let logged = true;
+    
+    let replaceIfLogged = "<p>You're already logged in</p> <button type='button' id='return_main'>Return to Home Page</button>";
+    
+    if(!logged) {
+        $("#form-div").replaceWith(replaceIfLogged);
+    }
+
+
+ //button takes to main page
+ $("#return_main").click(function() {
+    window.location.href = "../index.html";
+}); 
+
+
+
+});
