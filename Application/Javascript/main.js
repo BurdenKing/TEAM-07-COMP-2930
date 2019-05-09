cd74c7b9a2b96da38449138936334d5366903471
BurdenKing <lzheng20@my.bcit.ca>
2019-05-09 02:01:57 -0700

    Add testing markers and update coordinates for parking lot

--- Application/Javascript/main.js
+++ Application/Javascript/main.js
@@ -1,11 +1,11 @@
 
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
+  // The location of BCIT
   var BCIT = {lat: 49.2526785, lng: -123.0027276,17};
-  // The map, centered at Uluru
+  // The map, centered at BCIT
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: BCIT});
+      document.getElementById('map'), {zoom: 15, center: BCIT});
   // The marker, positioned at BCIT
   var marker = new google.maps.Marker({position: BCIT, map: map});
 }
