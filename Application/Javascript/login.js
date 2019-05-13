e2018ca2ab15158a7586eaf28e001d9d093e1e7b
unknown <pkhwong.92@gmail.com>
2019-05-13 10:22:58 -0700

    Modified login settings

--- Application/Javascript/login.js
+++ Application/Javascript/login.js
@@ -1,56 +1,35 @@
-$(document).ready(function() {
+//Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyC92FAonMbNaZiapSbs_A0RDzS0YPgpcMw",
+    authDomain: "parkaway-comp2930.firebaseapp.com",
+    databaseURL: "https://parkaway-comp2930.firebaseio.com",
+    projectId: "parkaway-comp2930",
+    storageBucket: "parkaway-comp2930.appspot.com",
+    messagingSenderId: "817259487218",
+    appId: "1:817259487218:web:c64c81d3cf0c5465"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 
-    //Gonna have to check server for this
-    let logged = true;
-    
-    //if user alreayd logged in, replace html with this
-    if(!logged) {
-        $("#form_id").replaceWith( "<p>You are already logged in </p> <button type = 'button' id='return_home'> return to home page</button>");
-    }
+//Get elements
+const txtEmail = document.getElementById('sign_in_email');
+const txtPassword = document.getElementById('sign_in_password');
+const btnSignin = document.getElementById('submit');
 
-
-   
-    //when submit clicked
-    $("#log_in_form").submit(function(e) {
+btnSignin.addEventListener('click', e => {
     e.preventDefault();
-    console.log('got here');
-    let var_user = document.getElementById('userName').value;
-    let var_pass = document.getElementById('password').value;
-    
-    //
-    // METHOD HERE THAT CHECKS IF EMAIL AND PASSWORD MATCHES AN ACCOUNT IN DATABASE
-    //
-    
-    if(!validateEmail(var_user)){
-        $("#error").text("invalid comination");
-        $("#error").css("color", "red");
-        e.preventDefault(); 
-    } else {
-        //check if the email exist in database
-        //if it exists, check if it matches the password
-        //if it matches, log the user in
-        window.location.href = "./main.html";
-    }
-});
 
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
 
-
-
-//checks if param is an email
-function validateEmail(email) {
-    let em = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return em.test(email);
-}
-
-//button takes to signup page
-$("#sign_up").click(function() {
-    window.location.href = "./signUp.html";
-});
-
-//button takes to home page
-$("#return_home").click(function() {
-    window.location.href = "./main.html";
-});
-
+    //Sign in
+    const promise = auth.signInWithEmailAndPassword(email,pass);
+    promise.then(e => {
+        alert("Success!");
+        setTimeout(window.location.href = "../index.html");
+    }).catch(e => {
+        console.log(e.message);
+    });
 
 });
