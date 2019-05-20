bb6954230ac8e495ea304cea5bc03f22bcb2d49f
unknown <pkhwong.92@gmail.com>
2019-05-20 16:57:52 -0700

    Modified header and sign-in pages

--- Application/Javascript/login-signup.js
+++ Application/Javascript/login-signup.js
@@ -1,6 +1,7 @@
 /* Switches bteween login and sign up*/
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
+const createAccLinkMobile = document.getElementById('create_acc_link_mobile');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
@@ -11,7 +12,13 @@ signInButton.addEventListener('click', () => {
     container.classList.remove('right-panel-active');
 });
 
-/* Firebase-Related Scripts */ 
+createAccLinkMobile.addEventListener('click', () => {
+    document.getElementById('sign_in_container_mobile').style.display = 'none';
+    document.getElementById('sign_up_container_mobile').style.display = 'block';
+});
+
+
+/* ----- Firebase-Related Scripts ------ */ 
 
 const firebaseConfig = {
     apiKey: "AIzaSyC92FAonMbNaZiapSbs_A0RDzS0YPgpcMw",
@@ -29,18 +36,83 @@ let database = firebase.database();
 
 /* ----- SIGN-UP-RELATED JAVASCRIPT ----- */
 
-//When submit is clicked.
-document.getElementById("create_acc_submit").addEventListener('click', e => {
+//When submit is clicked on desktop version.
+document.getElementById("create_acc_submit_desktop").addEventListener('click', e => {
 
     e.preventDefault();
 
     //Get values
-    let username = document.getElementById("create_acc_username").value;
-    let email = document.getElementById("create_acc_email").value;
-    let password = document.getElementById("create_acc_password").value;
-    let confirmpassword = document.getElementById("confirmpassword").value;
-    let firstname = document.getElementById("create_acc_firstname").value;
-    let lastname = document.getElementById("create_acc_lastname").value;
+    let username = document.getElementById("create_acc_username_desktop").value;
+    let email = document.getElementById("create_acc_email_desktop").value;
+    let password = document.getElementById("create_acc_password_desktop").value;
+    let confirmpassword = document.getElementById("confirmpassword_desktop").value;
+    let firstname = document.getElementById("create_acc_firstname_desktop").value;
+    let lastname = document.getElementById("create_acc_lastname_desktop").value;
+    let usernameIsTaken = false;
+
+    if (password == confirmpassword) {
+
+        database.ref('useraccount').once('value').then(snapshot => {
+            
+
+            //<!-- START CODE TO VALIDATE USERNAME WITH GOOGLE FIREBASE
+            let usernamedata = snapshot.val();
+            let keys = Object.keys(usernamedata);
+ 
+            for (let i = 0; i < keys.length; i++) {
+
+                let k = keys[i];
+                let usernamequery = usernamedata[k].username;
+
+                if (username == usernamequery) {
+                    usernameIsTaken = true;
+                }
+            }
+            // END CODE TO VALIDATE USERNAME WITH GOOGLE FIREBASE -->
+
+            if (usernameIsTaken) {
+                alert("The username is taken! Please enter a different username.")
+            } else {
+                //If result is false, then the user can create an account
+                auth.createUserWithEmailAndPassword(email, password).then(() => {
+
+                    alert("Success! You will be directed to the home page.");
+                    saveAccInfo(username, email, password, firstname, lastname);
+                    setTimeout(window.location.href = "../index.html");
+
+                }).catch(error => {
+                
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+
+                    if (errorCode == 'auth/weak-password') {
+                        alert("Please enter a longer password.");
+                    }  else if (errorCode == 'auth/email-already-in-use') {
+                        alert("Email is already taken. Please enter a different email.")
+                    } else {
+                        alert(errorMessage);
+                    }
+                });
+            
+            }
+        });
+    } else {
+        alert("Please check your password.");
+    }
+});
+
+//When submit is clicked on mobile version.
+document.getElementById("create_acc_submit_mobile").addEventListener('click', e => {
+
+    e.preventDefault();
+
+    //Get values
+    let username = document.getElementById("create_acc_username_mobile").value;
+    let email = document.getElementById("create_acc_email_mobile").value;
+    let password = document.getElementById("create_acc_password_mobile").value;
+    let confirmpassword = document.getElementById("confirmpassword_mobile").value;
+    let firstname = document.getElementById("create_acc_firstname_mobile").value;
+    let lastname = document.getElementById("create_acc_lastname_mobile").value;
     let usernameIsTaken = false;
 
     if (password == confirmpassword) {
@@ -118,15 +190,16 @@ function saveAccInfo(username, email, password, firstname, lastname) {
 
 }
 
+
 /* ----- LOGIN-RELATED JAVASCRIPT ----- */
 
-//when submit is clicked
-document.getElementById('loginsubmit').addEventListener('click', e => {
+//when submit is clicked on desktop version
+document.getElementById('loginsubmit_desktop').addEventListener('click', e => {
    
     e.preventDefault();
 
     const database = firebase.database();
-    const username = document.getElementById("sign_in_username").value;
+    const username = document.getElementById("sign_in_username_desktop").value;
     database.ref('useraccount').once('value').then(snapshot => {
 
         let data = snapshot.val();
@@ -149,7 +222,7 @@ document.getElementById('loginsubmit').addEventListener('click', e => {
             alert("Please check your username.");
         } else {
 
-            const pass = document.getElementById("sign_in_password").value;
+            const pass = document.getElementById("sign_in_password_desktop").value;
             const auth = firebase.auth();
 
             //Sign in
@@ -168,5 +241,55 @@ document.getElementById('loginsubmit').addEventListener('click', e => {
     });
 
 });
+
+//when submit is clicked on mobile version
+document.getElementById('loginsubmit_mobile').addEventListener('click', e => {
+   
+    e.preventDefault();
+
+    const database = firebase.database();
+    const username = document.getElementById("sign_in_username_mobile").value;
+    database.ref('useraccount').once('value').then(snapshot => {
+
+        let data = snapshot.val();
+        let keys = Object.keys(data);
+        let usernameIsValid = false;
+        let counter = 0;
+
+        for (let i = 0; i < keys.length; i++) {
+
+            let k = keys[i];
+            let usernamequery = data[k].username;
+
+            if (username == usernamequery) {
+                counter = k;
+                usernameIsValid = true;
+            }
+        }
+
+        if (!usernameIsValid) {
+            alert("Please check your username.");
+        } else {
+
+            const pass = document.getElementById("sign_in_password_mobile").value;
+            const auth = firebase.auth();
+
+            //Sign in
+            const promise = auth.signInWithEmailAndPassword(data[counter].email, pass);
+
+            promise.then(() => {
+                alert("Success!");
+                setTimeout(window.location.href = "../index.html");
+            }).catch(e => {
+                console.log(e.message);
+            }); 
+        }
+
+    }).catch(err => {
+        console.log(err);
+    });
+
+});
+
 
 
